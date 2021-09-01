d65325ffaa186f8f0683ae2bce0581fae4a3216c
Joel Beckum <joelbeckum@gmail.com>
2021-09-01 15:56:57 -0500

    Completed first 6 queries

--- PokiTestQueries.sql
+++ PokiTestQueries.sql
@@ -1,4 +1,43 @@
-﻿SELECT
-	Name
-FROM
-	Grade;
+﻿--SELECT
+--	Name
+--FROM
+--	Grade;
+
+--SELECT
+--	Name
+--FROM
+--	Emotion;
+
+--SELECT
+--	COUNT(Id) 'NumOfPoems'
+--FROM
+--	Poem;
+
+--SELECT DISTINCT TOP 76
+--	a.Name
+--FROM Author a
+--ORDER BY a.Name;
+
+--SELECT DISTINCT TOP 76
+--	a.Name, g.Name
+--FROM Author a
+--JOIN
+--	Grade g
+--ON
+--	a.GradeId = g.Id
+--ORDER BY a.Name;
+
+--SELECT DISTINCT TOP 76
+--	a.Name, 
+--	gn.Name 'Gender', 
+--	g.Name 'Grade'
+--FROM Author a
+--JOIN
+--	Grade g
+--ON
+--	a.GradeId = g.Id
+--JOIN
+--	Gender gn
+--ON
+--	a.GenderId = gn.Id
+--ORDER BY a.Name;
